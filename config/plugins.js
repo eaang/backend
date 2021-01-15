370c7e41cc99656af581b3fffd3e14096f843c15
Evan Ang <alethiologist@gmail.com>
2021-01-15 16:04:12 +0800

    removed the folder path

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,6 @@ module.exports = ({ env }) => ({
       cloud_name: env("CLOUDINARY_NAME"),
       api_key: env("CLOUDINARY_KEY"),
       api_secret: env("CLOUDINARY_SECRET"),
-      default_folder: env("CLOUDINARY_DEFAULT_FOLDER"),
     },
   },
 });
